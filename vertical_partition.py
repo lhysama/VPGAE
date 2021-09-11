db9e491bc6aaee5b0bcb1f4e2c84d9d849be6021
lihaoyang <476653880@qq.com>
2021-09-11 11:46:16 +0800

    Add TPC-DS experiments

--- vertical_partition.py
+++ vertical_partition.py
@@ -4,6 +4,7 @@ import VPGAE
 import column
 import row
 import optimal
+import my_cost_model
 import hillclimb
 
 import numpy as np
@@ -68,7 +69,7 @@ if __name__ == "__main__":
 		print("--------------------")
 '''
 
-'''
+
 # TPC-H experiments
 if __name__ == "__main__":
 	dataset_ = dataset.tpch_workload(10)
@@ -128,23 +129,30 @@ if __name__ == "__main__":
 	print("normalized reconstruction joins of ROW:", np.sum(number_of_joins(row_partitions_list, workload_list))/column_RJ)
 
 	print("--------------------")
-'''
 
+
+'''
 # TPC-DS experiments
 if __name__ == "__main__":
-	dataset_ = dataset.tpcds_workload(10)
+	dataset_ = dataset.tpcds_workload()
 	beam_costs = []
 	kmeans_costs = []
 	hill_costs = []
 	column_costs = []
 	row_costs = []
-
+	hyrise_costs = []
+	navathe_costs = []
+	o2p_costs = []
+	
 	beam_partitions_list = []
 	kmeans_partitions_list = []
 	hill_partitions_list = []
 	column_partitions_list = []
 	row_partitions_list = []
 	workload_list = []
+	hyrise_partitions_list = []
+	navathe_partitions_list = []
+	o2p_partitions_list = []
 
 	for i,data in enumerate(dataset_):
 		workload = Workload(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8])
@@ -169,17 +177,112 @@ if __name__ == "__main__":
 		row_partitions_list.append(row_partitions)
 		workload_list.append(workload)
 	
+	hyrise_partitions_list = [[[11], [10], [7], [13, 8, 6, 5, 4, 3, 2], [12], [9, 1]],
+						[[9, 8, 7, 6, 5], [2], [4, 3], [1]],
+						[[3, 1], [10], [15], [8], [9], [7], [11], [4], [28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 14, 13, 12, 6, 5, 2]],
+						[[3, 1], [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 2]],
+						[[3, 1], [6, 5, 4, 2]],
+						[[5, 4, 3, 1], [10, 9, 8, 7, 6, 2]],
+						[[3, 1], [2]],
+						[[3, 2, 1]],
+						[[15, 14], [22], [12], [13], [2], [1], [11], [20, 19, 18, 17, 16, 10, 7, 6, 5, 4, 3], [21], [9, 8]],
+						[[29, 27, 22, 21, 20, 19, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 5, 4, 3], [25], [24], [26], [23, 7], [18], [28], [2], [6, 1]],
+						[[12, 2], [7, 1], [31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 11, 10, 9, 8, 6, 5, 4, 3]],
+						[[10, 9], [2], [4, 3], [5], [11, 8], [1], [18, 17, 16, 15, 14, 13, 12, 7, 6]],
+						[[5, 1], [26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 4, 3, 2]],
+						[[5], [20, 19, 18, 17, 16, 15, 14, 13, 12, 8, 7, 6, 4, 2, 1], [11, 10, 9, 3]],
+						[[1], [2], [4], [3], [5]],
+						[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]],
+						[[15, 10, 1], [19, 18, 17, 16, 14, 13, 12, 11, 9, 8, 7, 6, 5, 4, 3, 2]],
+						[[1, 2, 3, 4, 5, 6, 7, 8, 9]],
+						[[4, 2, 1], [3]],
+						[[27, 12, 8, 1], [26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 11, 10, 9, 7, 6, 5, 4, 3, 2]],
+						[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]],
+						[[16, 15, 14, 3, 1], [34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 2]],
+						[[28, 21, 19, 17, 16, 5], [4], [15, 14, 12, 3], [34, 33, 32, 31, 30, 29, 27, 26, 25, 24, 23, 20, 18, 13, 11, 10, 9, 8, 7, 6, 2], [22, 1]],
+						[[20], [9], [4], [13], [3], [14], [1], [8], [23, 7], [2], [22, 21, 19, 18, 15, 12], [17], [16], [11, 5], [10, 6]]
+						]
+
+	for i in range(len(workload_list)):
+		hyrise_costs.append(my_cost_model.calculate_cost_fair(hyrise_partitions_list[i],workload_list[i]))
+
+	navathe_partitions_list = 	[[[13, 12], [11], [9], [10], [7], [2], [3], [4], [5], [6], [8], [1]],
+							[[9, 4, 3, 2], [5], [6], [7], [8], [1]],
+							[[28, 8], [10], [9], [7, 3], [4], [11], [15], [2], [5], [6], [12], [13], [14], [16], [17], [18], [19], [20], [21], [22], [23], [24], [25], [26], [27], [1]],
+							[[14], [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]],
+							[[6], [5, 4, 3, 2, 1]],
+							[[10], [5, 4, 3], [2], [6], [7], [8], [9], [1]],
+							[[3, 1], [2]],
+							[[3, 2, 1]],
+							[[20, 12], [15, 14], [8], [21], [9], [13], [11], [22], [2], [3], [4], [5], [6], [7], [10], [16], [17], [18], [19], [1]],
+							[[29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]],
+							[[31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]],
+							[[18, 3], [11, 8], [10, 9], [5], [4], [2], [6], [7], [12], [13], [14], [15], [16], [17], [1]],
+							[[26], [25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]],
+							[[20], [2], [11, 10, 9, 3], [4], [5], [6], [7], [8], [12], [13], [14], [15], [16], [17], [18], [19], [1]],
+							[[3, 2], [5, 4, 1]],
+							[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]],
+							[[19], [15, 10], [2], [3], [4], [5], [6], [7], [8], [9], [11], [12], [13], [14], [16], [17], [18], [1]],
+							[[1, 2, 3, 4, 5, 6, 7, 8, 9]],
+							[[3], [4, 2, 1]],
+							[[26], [27, 12, 8], [2], [3], [4], [5], [6], [7], [9], [10], [11], [13], [14], [15], [16], [17], [18], [19], [20], [21], [22], [23], [24], [25], [1]],
+							[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]],
+							[[34], [16, 15, 14, 3], [2], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13], [17], [18], [19], [20], [21], [22], [23], [24], [25], [26], [27], [28], [29], [30], [31], [32], [33], [1]],
+							[[34, 4], [28, 21, 19, 17, 16, 5], [22], [15, 14, 12, 3], [2], [6], [7], [8], [9], [10], [11], [13], [18], [20], [23], [24], [25], [26], [27], [29], [30], [31], [32], [33], [1]],
+							[[22, 9], [20, 13], [16], [23, 7], [11, 5], [14], [3], [8], [4], [10, 6], [17], [2], [12], [15], [18], [19], [21], [1]]
+							]
+
+	for i in range(len(workload_list)):
+		navathe_costs.append(my_cost_model.calculate_cost_fair(navathe_partitions_list[i],workload_list[i]))
+
+	o2p_partitions_list = 	[[[13, 12], [11], [9], [10], [7], [2], [3], [8, 6, 5, 4], [1]],
+						[[9, 4, 3, 2], [8, 7, 6, 5], [1]],
+						[[28, 8], [10], [9], [7], [3], [4], [11], [15, 2], [5], [6], [12], [13], [14], [16], [17], [18], [19], [20], [21], [22], [23], [24], [25], [26], [27], [1]],
+						[[14], [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]],
+						[[6], [5, 4, 3, 2, 1]],
+						[[10], [3], [4], [5], [9, 8, 7, 6, 2], [1]],
+						[[3, 1], [2]],
+						[[3],[2,1]],
+						[[20, 12], [14], [15], [8], [21], [9], [13], [11], [22], [2], [3], [4], [5], [6], [7], [10], [16], [17], [18], [19], [1]],
+						[[29, 7], [28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 6, 5, 4, 3, 2, 1]],
+						[[31, 12], [30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]],
+						[[18, 3], [11, 8], [10, 9], [5], [4], [2], [6], [7], [12], [13], [14], [15], [16], [17], [1]],
+						[[26], [25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]],
+						[[20, 2], [11, 10, 9, 3], [5, 4], [19, 18, 17, 16, 15, 14, 13, 12, 8, 7, 6, 1]],
+						[[3, 2], [5, 4, 1]],
+						[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]],
+						[[19], [18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]],
+						[[1, 2, 3, 4, 5, 6, 7, 8, 9]],
+						[[4, 2, 1], [3]],
+						[[26], [27, 12, 8], [25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 11, 10, 9, 7, 6, 5, 4, 3, 2], [1]],
+						[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]],
+						[[34], [3], [14], [15], [16], [2], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13], [17], [18], [19], [20], [21], [22], [23], [24], [25], [33, 32, 31, 30, 29, 28, 27, 26], [1]],
+						[[34, 4], [28, 21, 19, 17, 16, 5], [22], [3], [12], [14], [15], [2], [6], [7], [8], [9], [10], [11], [13], [18], [20], [23], [24], [25], [26], [27], [29], [30], [31], [32], [33], [1]],
+						[[22, 9], [13], [20], [16], [7], [23], [5], [14, 11], [3], [8], [4], [6], [10], [17], [2], [12], [15], [18], [19], [21], [1]]
+						]
+	
+	for i in range(len(workload_list)):
+		o2p_costs.append(my_cost_model.calculate_cost_fair(o2p_partitions_list[i],workload_list[i]))
+
 	print("VPGAE-B costs on 24 tables:", beam_costs)
 	print("VPGAE costs on 24 tables:", kmeans_costs)
 	print("HILLCLIMB costs on 24 tables:", hill_costs)
 	print("COLUMN costs on 24 tables:", column_costs)
 	print("ROW costs on 24 tables:", row_costs)
+
+	print("HYRISE costs on 24 tables:", hyrise_costs)
+	print("NAVATHE costs on 24 tables:", navathe_costs)
+	print("O2P costs on 24 tables:", o2p_costs)
 	
 	print("Unnecessary data read of VPGAE-B:", fraction_of_unnecessary_data_read(beam_partitions_list, workload_list))
 	print("Unnecessary data read of VPGAE:", fraction_of_unnecessary_data_read(kmeans_partitions_list, workload_list))
 	print("Unnecessary data read of HILLCLIMB:", fraction_of_unnecessary_data_read(hill_partitions_list, workload_list))
 	print("Unnecessary data read of COLUMN:", fraction_of_unnecessary_data_read(column_partitions_list, workload_list))
 	print("Unnecessary data read of ROW:", fraction_of_unnecessary_data_read(row_partitions_list, workload_list))
+
+	print("Unnecessary data read of HYRISE:", fraction_of_unnecessary_data_read(hyrise_partitions_list, workload_list))
+	print("Unnecessary data read of NAVATHE:", fraction_of_unnecessary_data_read(navathe_partitions_list, workload_list))
+	print("Unnecessary data read of O2P:", fraction_of_unnecessary_data_read(o2p_partitions_list, workload_list))
 
 	column_RJ = np.sum(number_of_joins(column_partitions_list, workload_list))
 	if column_RJ == 0:
@@ -191,8 +294,12 @@ if __name__ == "__main__":
 		print("normalized reconstruction joins of COLUMN:", column_RJ/column_RJ)
 		print("normalized reconstruction joins of ROW:", np.sum(number_of_joins(row_partitions_list, workload_list))/column_RJ)
 
-	print("--------------------")
+		print("normalized reconstruction joins of HYRISE:", np.sum(number_of_joins(hyrise_partitions_list, workload_list))/column_RJ)
+		print("normalized reconstruction joins of NAVATHE:", np.sum(number_of_joins(navathe_partitions_list, workload_list))/column_RJ)
+		print("normalized reconstruction joins of O2P:", np.sum(number_of_joins(o2p_partitions_list, workload_list))/column_RJ)
 
+	print("--------------------")
+'''
 
 '''
 # workload size experiments
