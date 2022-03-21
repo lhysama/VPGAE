199cec9c4071f5bd5a0051f4ee06ead7bac51c0e
lihaoyang-ruc <476653880@qq.com>
2022-03-21 23:56:14 +0800

    Update VPGAE.py
    Add row normalization in AGAE

--- VPGAE.py
+++ VPGAE.py
@@ -66,14 +66,13 @@ def beam_search(embedding,workload,origin_candidate_length,beam_search_width):
 		
 		if cost <= best_cost:
 			best_cost = cost
-			best_partitioning_scheme = partitioning_scheme
+			best_partitioning_scheme = restore_ps(partitioning_scheme, workload)
 
 		candidates.append([partitioning_scheme, cost])
 	
 	candidates.sort(key = lambda tup:tup[1])
-	
 	candidates = candidates[:origin_candidate_length]
-	
+
 	# start beam search
 	while True:
 		all_candidates = []
@@ -91,15 +90,8 @@ def beam_search(embedding,workload,origin_candidate_length,beam_search_width):
 					temp_cost = my_cost_model.calculate_cost_fair(origin_partitioning_scheme,workload)
 					
 					if temp_cost < best_cost:
-						if len(all_candidates) < beam_search_width:
-							all_candidates.append([temp_partitioning_scheme,temp_cost])
-							all_candidates.sort(key = lambda tup:tup[1])
-						else:
-							if temp_cost < all_candidates[beam_search_width-1][1]:
-								all_candidates.remove(all_candidates[beam_search_width-1])
-								all_candidates.append([temp_partitioning_scheme,temp_cost])
-								all_candidates.sort(key = lambda tup:tup[1])
-
+						all_candidates.append([temp_partitioning_scheme,temp_cost])
+			
 			# split a partition that includes more than 2 attributes
 			for i in range(len(partitioning_scheme)):
 				if len(partitioning_scheme[i]) > 1:
@@ -108,32 +100,22 @@ def beam_search(embedding,workload,origin_candidate_length,beam_search_width):
 						temp_partitioning_scheme.remove(partitioning_scheme[i])
 						temp_partitioning_scheme.append(partitioning_scheme[i][0:j])
 						temp_partitioning_scheme.append(partitioning_scheme[i][j:len(partitioning_scheme[i])])
-						
+
 						origin_partitioning_scheme = restore_ps(temp_partitioning_scheme, workload)
 						temp_cost = my_cost_model.calculate_cost_fair(origin_partitioning_scheme,workload)
 					
 						if temp_cost < best_cost:
-							if len(all_candidates) < beam_search_width:
-								all_candidates.append([temp_partitioning_scheme,temp_cost])
-								all_candidates.sort(key = lambda tup:tup[1])
-							else:
-								if temp_cost < all_candidates[beam_search_width-1][1]:
-									all_candidates.remove(all_candidates[beam_search_width-1])
-									all_candidates.append([temp_partitioning_scheme,temp_cost])
-									all_candidates.sort(key = lambda tup:tup[1])
+							all_candidates.append([temp_partitioning_scheme,temp_cost])
 
 		if len(all_candidates) == 0:
 			break
 		
-		if all_candidates[0][1] < best_cost:
-			best_cost = all_candidates[0][1]
-			best_partitioning_scheme = all_candidates[0][0]
-			candidates = all_candidates
-		else:
-			break
-
-	best_partitioning_scheme = restore_ps(best_partitioning_scheme, workload)
-	return best_cost,best_partitioning_scheme
+		all_candidates.sort(key = lambda tup:tup[1])		
+		best_cost = all_candidates[0][1]
+		best_partitioning_scheme = restore_ps(all_candidates[0][0], workload)
+		candidates = all_candidates[:beam_search_width]
+		
+	return best_cost, best_partitioning_scheme
 
 def simple_kmeans_search(embedding,workload):
 	kmax = embedding.shape[0]
@@ -165,11 +147,12 @@ def simple_kmeans_search(embedding,workload):
 			best_partitioning_scheme = partitioning_scheme
 	
 	best_partitioning_scheme = restore_ps(best_partitioning_scheme, workload)
+
 	return best_cost,best_partitioning_scheme
 
 # graph autoencoder
 class GAE(nn.Module):
-	def __init__(self,n_feat,n_hid,n_dim,k):
+	def __init__(self, n_feat, n_hid, n_dim, k):
 		super(GAE,self).__init__()
 		# encoder layer
 		self.gc1 = SGConv(n_feat, n_dim, K = k, bias = False, cached = True, add_self_loops = False)
@@ -178,28 +161,57 @@ class GAE(nn.Module):
 		self.layer1 = nn.Linear(n_dim, n_hid)
 		self.layer2 = nn.Linear(n_hid, n_feat,bias = False)
 
+	# def forward(self,data):
+	# 	x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
+	# 	# encoder
+	# 	x = self.gc1(x, edge_index, edge_attr)
+	# 	embedding = torch.tanh(x)
+
+	# 	# decoder
+	# 	x = self.layer1(embedding)
+	# 	x = F.relu(x)
+	# 	x = self.layer2(x)
+	# 	return x, embedding
+	
+	# def forward(self,data):
+	# 	x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
+	# 	# encoder
+	# 	x = self.gc1(x, edge_index, edge_attr)
+	# 	# row-norm (normalize each-row vector into a sphere)
+	# 	embedding = torch.div(x, torch.sqrt(torch.sum(x**2, 1, keepdims=True)))
+
+	# 	# decoder
+	# 	x = torch.mm(embedding, embedding.t())
+	# 	return x, embedding
+	
 	def forward(self,data):
 		x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
 		# encoder
-		embedding = self.gc1(x,edge_index,edge_attr)
+		x = self.gc1(x, edge_index, edge_attr)
+		# row-norm
+		embedding = torch.div(x, torch.sqrt(torch.sum(x**2, dim=1, keepdims=True)))
 
 		# decoder
 		x = F.relu(embedding)
 		x = self.layer1(x)
 		x = F.relu(x)
 		x = self.layer2(x)
-
 		return x, embedding
 
-def partition(algo_type,workload,n_hid,n_dim,k,origin_candidate_length=None, beam_search_width=None):
+def partition(algo_type, workload, n_hid, n_dim, k, origin_candidate_length=None, beam_search_width=None):
 	torch.manual_seed(seed = 777) # CPU pytorch seed
 	random.seed(777)
 
 	if workload.query_num == 0:
 		return 0, row.partition(workload)
 
-	# adjacent matrix of workload
-	adj = workload.affnity_matrix
+	# adjacent matrix
+	adj = workload.affinity_matrix
+	print("number of graph nodes:", adj.shape[0])
+	# graph preprocessing
+	for i in range(adj.shape[0]):
+		adj[i][i] = np.max(adj)
+	adj = adj/np.max(adj)
 
 	# initialize node feature as one-hot vector
 	features = torch.diag(torch.from_numpy(np.array([1.0 for i in range(adj.shape[1])],dtype=np.float32)))
@@ -207,11 +219,6 @@ def partition(algo_type,workload,n_hid,n_dim,k,origin_candidate_length=None, bea
 	# in autoencoder, the output is input
 	label_x = copy.deepcopy(features)
 	np.set_printoptions(threshold=1e6)
-
-	# graph preprocess
-	for i in range(adj.shape[0]):
-		adj[i][i] = np.max(adj)
-	adj = adj/np.max(adj)
 
 	edge_index = []
 	edge_attr = []
@@ -227,33 +234,31 @@ def partition(algo_type,workload,n_hid,n_dim,k,origin_candidate_length=None, bea
 	data = Data(x=features, edge_index=edge_index.t().contiguous(), edge_attr=edge_attr)
 
 	model = GAE(features.shape[0],n_hid,n_dim,k)
-
-	optimizer = optim.Adam(model.parameters(), lr = 0.01, weight_decay = 5e-4)
+	optimizer = optim.Adam(model.parameters(), lr = 1e-5, weight_decay = 5e-4)
 	
 	min_loss = float('inf')
 	# early-stop
-	patience = 15
+	patience = 20
 	step = 0
 	optimal_model = None
 
-	row_list = [i for i in range(features.shape[1])]
-	
+	indices = [i for i in range(features.shape[1])]
 	for i in range(1000):
-		# if early-stop is triggered
+		# early-stop is triggered
 		if(step >= patience):
 			break
 
-		if len(row_list) > 1:
+		if len(indices) > 1:
 			# randomly split train set and validation set
-			train_index = random.sample(row_list, max(1,int((1/4)*features.shape[1])))
+			train_index = random.sample(indices, max(1, math.ceil((features.shape[1]/4))))
+			
 			valid_index = []
-
-			for ele in row_list:
+			for ele in indices:
 				if ele not in train_index:
 					valid_index.append(ele)
 		else: # there is only one node in the graph
-			train_index = [0]
-			valid_index = [0]
+			train_index = indices
+			valid_index = indices
 
 		model.train()
 		optimizer.zero_grad()
@@ -262,7 +267,7 @@ def partition(algo_type,workload,n_hid,n_dim,k,origin_candidate_length=None, bea
 		train_loss = my_loss(output[train_index],label_x[train_index])
 		train_loss.backward()
 		optimizer.step()
-
+		
 		model.eval()
 		with torch.no_grad():
 			output, embedding = model(data)
@@ -274,13 +279,19 @@ def partition(algo_type,workload,n_hid,n_dim,k,origin_candidate_length=None, bea
 			optimal_model = copy.deepcopy(model)
 		else:
 			step += 1
-
-	model = optimal_model
+	
+	# model = copy.deepcopy(optimal_model)
 	model.eval()
 	with torch.no_grad():
 		output,embedding = model(data)
 	embedding = embedding.numpy().astype(np.float64)
-
+	
+	# similarity = np.dot(embedding, embedding.T)
+	# visualization.heatmap(adj, [str(i) for i in range(adj.shape[0])], [str(i) for i in range(adj.shape[1])], 
+	# 					"affinity matrix", "affinity between attributes", "attribute id")
+	# visualization.heatmap(similarity, [str(i) for i in range(adj.shape[0])], [str(i) for i in range(adj.shape[1])],
+	# 					"similarity matrix", "similarity between learned feature vectors", "learned feature vector id")
+	
 	if algo_type == "VPGAE-B":
 		beam_cost,beam_partitions = beam_search(embedding, workload, origin_candidate_length, beam_search_width)
 		
@@ -290,6 +301,5 @@ def partition(algo_type,workload,n_hid,n_dim,k,origin_candidate_length=None, bea
 		kmeans_cost,kmeans_partitions = simple_kmeans_search(embedding, workload)
 
 		return kmeans_cost,kmeans_partitions
-
 	else:
 		print("no corresponding variant.")
